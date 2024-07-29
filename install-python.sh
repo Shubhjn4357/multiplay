1a97bc3612e8b57de404cfc496b3c915f157cd11
shubhamjain.com.in <shubhamjain.com.in@gmail.com>
2024-07-29 17:26:23 +0000

    add bash script

---
+++ install-python.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Install pyenv
+curl https://pyenv.run | bash
+
+# Add pyenv to PATH and initialize
+export PATH="$HOME/.pyenv/bin:$HOME/.pyenv/shims:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+
+# Install Python 3.9.6
+pyenv install 3.9.6
+pyenv global 3.9.6
+
+# Verify Python installation
+python --version
+
+# Continue with npm install
+npm install
